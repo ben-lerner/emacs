ee4c372f5f45e87d50748552dde90c9643e7bc9f
Ben Lerner <bl@nlglgn.com>
2017-10-23 22:42:48 -0400

    org-shifttab

--- my-org-mode.el
+++ my-org-mode.el
@@ -18,6 +18,7 @@
 (bind-key "<left>" 'org-do-promote org-mode-map)
 (bind-key "<right>" 'org-do-demote org-mode-map)
 (bind-key "M-`" 'org-cycle org-mode-map)
+(bind-key "C-M-`" 'org-shifttab org-mode-map)
 (bind-key "<tab>" 'org-do-demote org-mode-map)
 (bind-key "<S-tab>" 'org-do-promote org-mode-map)
 (bind-key "M-<left>" 'org-promote-subtree org-mode-map)
