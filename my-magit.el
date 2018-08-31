1a42da268bae62ac4d5fe7f12fbd0a5bddc871a2
Ben Lerner <bl@nlglgn.com>
2018-08-31 19:07:07 -0400

    Add save-and-stage-file.

--- my-magit.el
+++ my-magit.el
@@ -1,2 +1,9 @@
 (require 'magit)
 (bind-key "C-c g" 'magit-status)
+
+(defun save-and-stage-file ()
+  (interactive)
+  (save-buffer)
+  (magit-stage-file buffer-file-name))
+
+(bind-key* "C-x C-s" 'save-and-stage-file)
