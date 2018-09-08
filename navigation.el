ffe4534c09fdaa6fb9d2ee08d4f79f35d932b866
Ben Lerner <bl@nlglgn.com>
2018-09-08 16:17:34 -0400

    Drop "." and ".." from ivy file autocomplete.

--- navigation.el
+++ navigation.el
@@ -143,4 +143,8 @@
               (cider-jack-in))))       ; goto-repl
 (bind-key* "M-g M-f" 'ffap-with-line)
 
+;; file autocomplete
 (ivy-mode)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+(setq ivy-extra-directories nil)  ;; don't show . and ..
