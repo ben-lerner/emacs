20ae90c468c0c10f10b6ce98b2f479286dc4ada1
Ben Lerner <ben@bl.dev>
2020-09-13 01:16:42 -0400

    Add latex mode with custom command for dse.

---
+++ my_latex.el
@@ -0,0 +1,19 @@
+(require 'tex-mode)
+;; Run pdflatex on current buffer; defined in bash_scripts
+;; Run make.sh for dse.
+(defun ltx ()
+  (interactive)
+  (save-buffer)
+  (let ((temp-buffer "*latex-output*"))
+    (with-output-to-temp-buffer
+     temp-buffer
+     (shell-command
+      (if (string-match "/distributed-systems-for-everyone/" default-directory)
+          "cd ..; bash make.sh && open -g book.pdf"  ;; -g = don't change window focus
+          "ltx")
+      temp-buffer  ;; stdout
+      "*Mesages*"  ;; stderr
+      ))
+    (pop-to-buffer temp-buffer)))
+
+(bind-key "C-c C-c" 'ltx tex-mode-map)
